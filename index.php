f8cc863318cb6207f0d67a1072cb919c74d18218
engi <enric.gimeno.vilanova@gmail.com>
2018-05-11 15:53:33 +0200

    Adding new funcionallity to the map

--- index.php
+++ index.php
@@ -5,11 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Here hackathon</title>
 
     <!-- Bootstrap -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/BootSideMenu.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" 
+      href="http://js.api.here.com/v3/3.0/mapsjs-ui.css" />
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -22,23 +24,33 @@
     <div class="leftmenu">
       <h4>Left Panel</h4>
     </div>
-    
-    <div class="here-maps" id="mapContainer"></div>
-    <!-- <div id="mapContainer" style="width: 100%; height: 400px; background: grey" /> -->
+
+    <div id="mapContainer" style="width: 100%; height: 800px; background: grey" /> 
     <!-- <div class="container">
     <div class="here-maps" id="mapContainer"></div>
+    </div> -->
+    <!--     <div id="main">
+      <div id="mapContainer"></div>
     </div> -->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- PANEL LIBRARY -->
     <script src="js/BootSideMenu.js"></script>
+
+    <!-- HERE MAPS LIBRARY -->
     <script src="http://js.api.here.com/v3/3.0/mapsjs-core.js"
       type="text/javascript" charset="utf-8"></script>
     <script src="http://js.api.here.com/v3/3.0/mapsjs-service.js"
       type="text/javascript" charset="utf-8"></script>
     </script>
+    <script src="http://js.api.here.com/v3/3.0/mapsjs-mapevents.js" 
+     type="text/javascript" charset="utf-8"></script>
+     <script src="http://js.api.here.com/v3/3.0/mapsjs-ui.js" 
+      type="text/javascript" charset="utf-8"></script>
+
     <!--My Scripts-->
     <script src="js/leftpanel.js"></script>
     <script src="js/map.js"></script>
--- js/map.js
+++ js/map.js
@@ -12,6 +12,24 @@ var platform = new H.service.Platform({
       document.getElementById('mapContainer'),
       maptypes.normal.map,
       {
-        zoom: 10,
-        center: { lng: 13.4, lat: 52.51 }
+        zoom: 13,
+        center: { lng: -0.38, lat: 39.47 }
       });
+      
+
+      // Create the default UI:
+      var ui = H.ui.UI.createDefault(map, maptypes, 'es-ES');
+
+       // Enable the event system on the map instance:
+      var mapEvents = new H.mapevents.MapEvents(map);
+
+      // Add event listener:
+      map.addEventListener('tap', function(evt) {
+        // Log 'tap' and 'mouse' events:
+        console.log(evt.type, evt.currentPointer.type); 
+      });
+     
+      // Instantiate the default behavior, providing the mapEvents object: (Zoom and move map)
+      var behavior = new H.mapevents.Behavior(mapEvents);
+
+
